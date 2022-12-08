f16dcbed9527a8424584369b73149ea5b92f7ed0
Yusif Serdarli <srdarliyusif@gmail.com>
2022-12-09 03:13:07 +0400

    added features

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,13 @@ const brands = document.querySelector('.brands');
 const cancel = document.getElementById('cancel');
 const searchIcon = document.getElementById('search-icon');
 const searchInput = document.getElementById('search-input');
+const scrollToIcon = document.querySelector('.to-head');
+const followUs = document.querySelector('.follow-us');
+const clBrands = document.querySelector('.cl-brands');
+const exploreWorld = document.querySelector('.explore-world');
+const followUsLink = document.querySelector('.follow-us-link');
+const clBrandsLink = document.querySelector('.cl-brands-link');
+const exploreWorldLink = document.querySelector('.explore-world-link');
 // For hamburger icon click
 hamburger.addEventListener('click',() => {
     hamburger.classList.add('d-none');
@@ -25,7 +32,50 @@ cancel.addEventListener('click',() => {
 searchIcon.addEventListener('click',() => {
     searchInput.classList.toggle('d-block');
 })
-
+// Activating second menu
 plusIcon.addEventListener('click',() => {
     brands.classList.toggle('d-none');
+})
+// Scroll to
+scrollToIcon.addEventListener('click',() => {
+    window.scrollTo(0,0)
+})
+// For COMPACT LIST
+followUsLink.addEventListener('click',() => {
+    followUsLink.classList.add('active');
+    clBrandsLink.classList.remove('active');
+    exploreWorldLink.classList.remove('active');
+
+    clBrands.classList.add('d-none');
+    clBrands.classList.remove('d-flex');
+    exploreWorld.classList.add('d-none');
+    exploreWorld.classList.remove('d-flex');
+    followUs.classList.add('d-flex');
+    followUs.classList.remove('d-none');
+
+})
+clBrandsLink.addEventListener('click',() => {
+    clBrandsLink.classList.add('active');
+    followUsLink.classList.remove('active');
+    exploreWorldLink.classList.remove('active');
+
+    followUs.classList.add('d-none');
+    followUs.classList.remove('d-flex');
+    exploreWorld.classList.add('d-none');
+    exploreWorld.classList.remove('d-flex');
+    clBrands.classList.add('d-flex');
+    clBrands.classList.remove('d-none');
+    
+})
+exploreWorldLink.addEventListener('click',() => {
+    exploreWorldLink.classList.add('active');
+    clBrandsLink.classList.remove('active');
+    followUsLink.classList.remove('active');
+
+    clBrands.classList.add('d-none');
+    clBrands.classList.remove('d-flex');
+    followUs.classList.add('d-none');
+    followUs.classList.remove('d-flex');
+    exploreWorld.classList.add('d-flex');
+    exploreWorld.classList.remove('d-none');
 })
